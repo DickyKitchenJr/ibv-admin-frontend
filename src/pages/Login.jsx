cf47b9242c1ee4ecb2bb00b1dc434ec9b6909162
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-12-05 16:25:23 -0500

    Create Login.jsx
    started building out log in form

---
+++ src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import '../styles/Login.css'
+
+
+function Login() {
+
+  return (
+    <main>
+      <div className='login-div'>
+        <form action="POST" className='login-form'>
+          <div className='login-form-inner-div'>
+            <p className='login-p'>Log In</p>
+          <label htmlFor="username" className='login-label'>User Name:</label>
+          <input type="text" id='username'/>
+          <label htmlFor="password" className='login-label'>Password</label>
+          <input type="password" id='password' />
+          </div>
+        </form>
+      </div>
+    </main>
+  )
+}
+
+export default Login
