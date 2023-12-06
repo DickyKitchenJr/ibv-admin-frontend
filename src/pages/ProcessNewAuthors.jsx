ca362292da0ac88ff36d34ba7a1361759ebbfdc3
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-12-06 14:14:55 -0500

    Create ProcessNewAuthors.jsx

---
+++ src/pages/ProcessNewAuthors.jsx
@@ -0,0 +1,12 @@
+import AddToAuthorList from "../components/AddToAuthorList"
+
+function ProcessNewAuthors() {
+  return (
+    <>
+    <h1>Confirm New Authors</h1>
+      <AddToAuthorList />
+    </>
+  )
+}
+
+export default ProcessNewAuthors
