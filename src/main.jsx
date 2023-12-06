980f883cf0e0353cab9ca9aed3109181e3ea61de
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-12-06 14:14:59 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './styles/index.css'
 import Login from './pages/Login.jsx';
+import ProcessNewAuthors from './pages/ProcessNewAuthors.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
   },
+  {
+    path: "addauthors",
+    element: <ProcessNewAuthors />
+  }
 ]);
 
 
