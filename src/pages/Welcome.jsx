01c7f1680f89a4d7507bd542b9e94ae1cfd0487f
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-12-18 11:13:18 -0500

    Create Welcome.jsx

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import "../styles/Welcome.css";
+
+function Welcome() {
+  return (
+    <>
+      <h1 className="title">Welcome To The Inner Vault</h1>
+      <h2 className="title">How Would You Like To Help?</h2>
+      <main className="welcome">
+        <NavLink to="/addauthors" className="welcome-tab">
+          Process Applications
+        </NavLink>
+        <NavLink to="/modify" className="welcome-tab">
+            Modify Listing
+        </NavLink>
+        <NavLink to="/verify" className="welcome-tab">
+            Verify Listings
+        </NavLink>
+      </main>
+    </>
+  );
+}
+
+export default Welcome;
