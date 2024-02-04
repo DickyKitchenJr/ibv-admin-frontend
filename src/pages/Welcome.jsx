141b9f279971e63050d828492492608c5e3a9b58
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-03 20:00:15 -0500

    Update Welcome.jsx
    added a conditional statement to account for what access level the user has

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,24 +1,51 @@
 import { NavLink } from "react-router-dom";
 import "../styles/Welcome.css";
 import Logout from "../components/Logout";
+import { AuthContext } from "../context/AuthProvider";
+import { useContext } from "react";
 
 function Welcome() {
+  const { userAccessLevel } = useContext(AuthContext);
+
   return (
     <>
       <Logout />
       <h1 className="title">Welcome To The Inner Vault</h1>
       <h2 className="title">How Would You Like To Help?</h2>
-      <main className="welcome">
-        <NavLink to="/addauthors" className="welcome-tab">
-          Process Applications
-        </NavLink>
-        <NavLink to="/modify" className="welcome-tab">
-          Modify Listing
-        </NavLink>
-        <NavLink to="/verify" className="welcome-tab">
-          Verify Listings
-        </NavLink>
-      </main>
+      {userAccessLevel === "admin" ? (
+        <>
+          {" "}
+          <main className="welcome">
+            <NavLink to="/addauthors" className="welcome-tab">
+              Process Applications
+            </NavLink>
+            <NavLink to="/modify" className="welcome-tab">
+              Modify Listing
+            </NavLink>
+            <NavLink to="/verify" className="welcome-tab">
+              Verify Listings
+            </NavLink>
+            <NavLink to="/delete" className="welcome-tab">
+              Delete Authors
+            </NavLink>
+          </main>{" "}
+        </>
+      ) : (
+        <>
+          {" "}
+          <main className="welcome">
+            <NavLink to="/addauthors" className="welcome-tab">
+              Process Applications
+            </NavLink>
+            <NavLink to="/modify" className="welcome-tab">
+              Modify Listing
+            </NavLink>
+            <NavLink to="/verify" className="welcome-tab">
+              Verify Listings
+            </NavLink>
+          </main>{" "}
+        </>
+      )}
     </>
   );
 }
