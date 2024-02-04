cdf59ec9e0ac1a04c4e669ca3a034d73f141b54b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-03 19:55:03 -0500

    Create DeleteAuthors.jsx
    added page to be used for deleting authors

---
+++ src/pages/DeleteAuthors.jsx
@@ -0,0 +1,12 @@
+import Logout from "../components/Logout";
+
+function DeleteAuthors() {
+  return (
+    <>
+      <Logout />
+      Delete Authors
+    </>
+  );
+}
+
+export default DeleteAuthors;
