adc0d5b286e7b70d7f7246ace84a804007f6727c
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-06 13:17:30 -0500

    added styling for Logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,14 +1,19 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
+import "../styles/Logout.css";
 
 function Logout() {
-  const {handleLogout} = useContext(AuthContext)
-  
-  const handleClick = (e) =>{
-    handleLogout(e);
-  }
+  const { handleLogout } = useContext(AuthContext);
 
-  return <button onClick={handleClick}>Log Out</button>;
+  const handleClick = (e) => {
+    handleLogout(e);
+  };
+
+  return (
+    <button className="logout" onClick={handleClick}>
+      Log Out
+    </button>
+  );
 }
 
 export default Logout;
