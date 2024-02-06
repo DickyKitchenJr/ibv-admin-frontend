cfa1af1ff9cc4ae7c0652b0f1fd584536756021b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-06 13:37:31 -0500

    Update Login.jsx
    - added comment for clarification
    
    - formatted on save

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,8 @@ function Login() {
     username: "",
     password: "",
   });
-  const { handleLogin, failedCount, failedLogin, isLoggedIn } = useContext(AuthContext)
+  const { handleLogin, failedCount, failedLogin, isLoggedIn } =
+    useContext(AuthContext);
 
   // Username and Password handler
   const handleInputChange = (e) => {
@@ -23,64 +24,77 @@ function Login() {
   };
 
   return (
-      <main>
-      {isLoggedIn === false ? <> {failedCount < 10 ? (
+    <main>
+      {/* if user is not logged in */}
+      {isLoggedIn === false ? (
         <>
+        {/* if user has not failed to log in >= 10 times */}
           {" "}
-          <div className="login-div">
-            <form action="POST" className="login-form" onSubmit={handleSubmit}>
-              <div className="login-form-inner-div">
-                {failedLogin ? (
-                  <p className="failed-login">Incorrect Name Or Password</p>
-                ) : null}
-                <p className="login-p">Log In</p>
-                <label htmlFor="username" className="login-label">
-                  User Name:
-                </label>
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  autoComplete="on"
-                  value={formData.username}
-                  onChange={handleInputChange}
-                />
-                <br />
-                <label htmlFor="password" className="login-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                />
-                <br />
-                <button type="submit" className="login-submit">
-                  Log In
-                </button>
+          {failedCount < 10 ? (
+            <>
+              {" "}
+              <div className="login-div">
+                <form
+                  action="POST"
+                  className="login-form"
+                  onSubmit={handleSubmit}
+                >
+                  <div className="login-form-inner-div">
+                    {failedLogin ? (
+                      <p className="failed-login">Incorrect Name Or Password</p>
+                    ) : null}
+                    <p className="login-p">Log In</p>
+                    <label htmlFor="username" className="login-label">
+                      User Name:
+                    </label>
+                    <input
+                      type="text"
+                      id="username"
+                      name="username"
+                      autoComplete="on"
+                      value={formData.username}
+                      onChange={handleInputChange}
+                    />
+                    <br />
+                    <label htmlFor="password" className="login-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleInputChange}
+                    />
+                    <br />
+                    <button type="submit" className="login-submit">
+                      Log In
+                    </button>
+                  </div>
+                </form>
+              </div>{" "}
+            </>
+          ) : (
+            <>
+              <div className="rejected">
+                <iframe
+                  src="https://giphy.com/embed/ftqLysT45BJMagKFuk"
+                  width="480"
+                  height="405"
+                  allowFullScreen
+                ></iframe>
+                <p>
+                  <a href="https://giphy.com/gifs/chicken-bro-ftqLysT45BJMagKFuk">
+                    via GIPHY
+                  </a>
+                </p>
               </div>
-            </form>
-          </div>{" "}
+            </>
+          )}{" "}
         </>
       ) : (
-        <><div className="rejected">
-          <iframe
-            src="https://giphy.com/embed/ftqLysT45BJMagKFuk"
-            width="480"
-            height="405"
-            allowFullScreen
-          ></iframe>
-          <p>
-            <a href="https://giphy.com/gifs/chicken-bro-ftqLysT45BJMagKFuk">
-              via GIPHY
-            </a>
-          </p>
-        </div>
-        </>
-      )} </> : <Welcome/> }
-      
+        <Welcome />
+      )}
     </main>
   );
 }
