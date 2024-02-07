6fb5f792a223debcc661c794443cd4c24a37ddbb
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-07 11:15:15 -0500

    Update AddUsers.jsx
    added logic for handling form change

--- src/pages/AddUsers.jsx
+++ src/pages/AddUsers.jsx
@@ -2,10 +2,31 @@ import Logout from "../components/Logout";
 import "../styles/Pages.css";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function AddUsers() {
   const { userAccessLevel } = useContext(AuthContext);
+  const [formData, setFormData] = useState({
+    userName: null,
+    password: null,
+    accessLevel: "helper",
+  });
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleCheckbox = (e) => {
+    if (isChecked === false) {
+      setIsChecked(true);
+      setFormData({ ...formData, accessLevel: "admin" });
+    } else {
+      setIsChecked(false);
+      setFormData({ ...formData, accessLevel: "helper" });
+    }
+  };
 
   const unauthorizedUser = () => {
     if (userAccessLevel !== "admin") {
@@ -17,7 +38,7 @@ function AddUsers() {
     <>
       {/* check if user is authorized, and if not deny access and send them to the login */}
       {unauthorizedUser()}
-      
+
       <nav className="top-bar">
         <NavLink to="/welcome" className="back">
           &#129092; Back
