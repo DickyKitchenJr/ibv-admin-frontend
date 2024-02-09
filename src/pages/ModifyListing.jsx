dcee71e99d3bbd84338923b0074cb7ed032791d6
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-09 10:45:13 -0500

    Update ModifyListing.jsx
    added logic for handling form and  data provided by successful fetch

--- src/pages/ModifyListing.jsx
+++ src/pages/ModifyListing.jsx
@@ -2,10 +2,54 @@ import Logout from "../components/Logout";
 import "../styles/Pages.css";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function ModifyListing() {
   const { userAccessLevel } = useContext(AuthContext);
+  const [displayInfo, setDisplayInfo] = useState("getAuthor");
+  const [authorEmail, setAuthorEmail] = useState({ email: "" });
+  const [authorListing, setAuthorListing] = useState([]);
+  const apiGetAuthorAddress = import.meta.env.VITE_API_AUTHOR_BY_EMAIL_ADDRESS;
+
+  const handleEmailInput = (e) => {
+    const { value } = e.target;
+    setAuthorEmail({ email: value });
+  };
+
+  const handleEmailSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        apiGetAuthorAddress + `/${authorEmail.email}`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      if (response.ok) {
+        //set author listing
+        const result = await response.json();
+        setAuthorListing(result.author);
+        console.log(result);
+        //change display to show modify author form
+        handleDisplayInfo();
+      } else {
+        alert("Error. Author Search Failed.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleDisplayInfo = () => {
+    if (displayInfo === "getAuthor") {
+      setDisplayInfo("modifyAuthor");
+    } else {
+      setDisplayInfo("getAuthor");
+    }
+  };
 
   const unauthorizedUser = () => {
     if (userAccessLevel !== "admin" && userAccessLevel !== "helper") {
