479dba639cbe67d53ed223730ce0749ce7d51a9a
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-09 10:42:11 -0500

    Update ModifyListing.jsx
    added structure/styling for form to retrieve author listing by email

--- src/pages/ModifyListing.jsx
+++ src/pages/ModifyListing.jsx
@@ -17,7 +17,7 @@ function ModifyListing() {
     <>
       {/* check if user is authorized, and if not deny access and send them to the login */}
       {unauthorizedUser()}
-      
+
       <nav className="top-bar">
         <NavLink to="/welcome" className="back">
           &#129092; Back
@@ -26,6 +26,27 @@ function ModifyListing() {
       </nav>
 
       <h1 className="title">Modify Listing</h1>
+
+      <main className="main">
+        <form className="author-info" onSubmit={handleEmailSubmit}>
+          <p className="center-text">Only Change Requested Information</p>
+          <br />
+          <label htmlFor="email">Author's Email:</label>
+          <input
+            className="user-input"
+            type="text"
+            name="email"
+            id="email"
+            value={authorEmail.email}
+            onChange={handleEmailInput}
+          />
+          <br />
+          <br />
+          <div className="submit">
+            <button className="button">Submit</button>
+          </div>
+        </form>
+      </main>
     </>
   );
 }
