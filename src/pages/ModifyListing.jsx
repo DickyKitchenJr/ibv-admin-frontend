0aae6952d09d4d68b1f2e1ce6610573134ee10a5
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-12 19:37:28 -0500

    Update ModifyListing.jsx

--- src/pages/ModifyListing.jsx
+++ src/pages/ModifyListing.jsx
@@ -2,13 +2,13 @@ import Logout from "../components/Logout";
 import "../styles/Pages.css";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 
 function ModifyListing() {
   const { userAccessLevel } = useContext(AuthContext);
   const [displayInfo, setDisplayInfo] = useState("getAuthor");
   const [authorEmail, setAuthorEmail] = useState({ email: "" });
-  const [authorListing, setAuthorListing] = useState([]);
+  const [authorListing, setAuthorListing] = useState({});
   const [dataToModify, setDataToModify] = useState({
     name: false,
     authorEmail: false,
@@ -17,6 +17,8 @@ function ModifyListing() {
     links: false,
     authorBio: false,
   });
+  const [userChoice, setUserChoice] = useState("");
+
   const apiGetAuthorAddress = import.meta.env.VITE_API_AUTHOR_BY_EMAIL_ADDRESS;
 
   const handleDisplayInfo = () => {
@@ -47,9 +49,17 @@ function ModifyListing() {
       );
 
       if (response.ok) {
-        //set author listing
         const result = await response.json();
-        setAuthorListing(result.author);
+        //change initial result of umbrellaGenre and subGenre from strings to arrays
+        const parseUmbrellaGenre = JSON.parse(result.author.umbrellaGenre);
+        const parseSubGenre = JSON.parse(result.author.subGenre);
+        //create object from author listing in results
+        const listing = result.author;
+        //modify object to include parsed data
+        listing.umbrellaGenre = parseUmbrellaGenre;
+        listing.subGenre = parseSubGenre;
+        //set authorListing as listing object
+        setAuthorListing(listing);
         //change display to show modify author form
         handleDisplayInfo();
       } else {
@@ -67,6 +77,52 @@ function ModifyListing() {
       ...prevLinks,
       [checkboxName]: !prevLinks[checkboxName],
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setAuthorListing({ ...authorListing, [name]: value });
+  };
+
+  const handleGenreChange = (e) => {
+    const genreValue = e.target.value;
+    const updatedGenres = [...authorListing.umbrellaGenre];
+    if (e.target.checked) {
+      // If checkbox is checked, add the genre to the array
+      updatedGenres.push(genreValue);
+    } else {
+      // If checkbox is unchecked, remove the genre from the array
+      const genreIndex = updatedGenres.indexOf(genreValue);
+      if (genreIndex !== -1) {
+        updatedGenres.splice(genreIndex, 1);
+      }
+    }
+    setAuthorListing({ ...authorListing, umbrellaGenre: updatedGenres });
+  };
+
+  const handleAddSubGenre = (e) => {
+    e.preventDefault();
+    setAuthorListing({
+      ...authorListing,
+      subGenre: [...authorListing.subGenre, userChoice],
+    });
+    // clear userChoice
+    setUserChoice("");
+  };
+
+  const handleRemoveSubGenre = (index) => {
+    const updatedSubGenre = authorListing.subGenre.filter(
+      (_, i) => i !== index
+    );
+    setAuthorListing({
+      ...authorListing,
+      subGenre: updatedSubGenre,
+    });
+  };
+
+  const handleUserChoice = (e) => {
+    const { value } = e.target;
+    setUserChoice(value);
   };
 
   const unauthorizedUser = () => {
@@ -118,6 +174,7 @@ function ModifyListing() {
             <br />
             <p>Select What To Change:</p>
             <div>
+              {/* Name Section */}
               <label htmlFor="name">Name</label>{" "}
               <input
                 type="checkbox"
@@ -127,6 +184,33 @@ function ModifyListing() {
                 checked={dataToModify.name}
               />
               <br />
+              {!dataToModify.name ? null : (
+                <>
+                  <label htmlFor="firstName">First Name:</label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    className="user-input"
+                    defaultValue={authorListing.firstName}
+                    value={authorListing.firstName}
+                    onChange={handleInputChange}
+                  />
+                  <br />
+                  <label htmlFor="lastName">Last Name:</label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    className="user-input"
+                    defaultValue={authorListing.lastName}
+                    value={authorListing.lastName}
+                    onChange={handleInputChange}
+                  />
+                  <br />
+                </>
+              )}
+              {/* Email Section */}
               <label htmlFor="authorEmail">Email</label>{" "}
               <input
                 type="checkbox"
@@ -136,6 +220,24 @@ function ModifyListing() {
                 checked={dataToModify.authorEmail}
               />
               <br />
+              {!dataToModify.authorEmail ? null : (
+                <>
+                  <label htmlFor="email" className="for-accessibility">
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    className="user-input"
+                    defaultValue={authorListing.email}
+                    value={authorListing.email}
+                    onChange={handleInputChange}
+                  />
+                  <br />
+                </>
+              )}
+              {/* Umbrella Genre Section */}
               <label htmlFor="genre">Umbrella Genre</label>{" "}
               <input
                 type="checkbox"
@@ -145,7 +247,165 @@ function ModifyListing() {
                 checked={dataToModify.genre}
               />
               <br />
-              <label htmlFor="nicheGenre">Sub-Genre</label>{" "}
+              {!dataToModify.genre ? null : (
+                <>
+                  <label htmlFor="umbrellaGenre" className="for-accessibility">
+                    Umbrella Genre
+                  </label>
+                  <div id="umbrellaGenre" className="umbrella-genre">
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="romance"
+                        name="umbrellaGenre"
+                        value="romance"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "romance"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="romance">Romance</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="fantasy"
+                        name="umbrellaGenre"
+                        value="fantasy"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "fantasy"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="fantasy">Fantasy</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="thriller"
+                        name="umbrellaGenre"
+                        value="thriller"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "thriller"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="thriller">Thriller</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="scifi"
+                        name="umbrellaGenre"
+                        value="scifi"
+                        checked={authorListing.umbrellaGenre.includes("scifi")}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="scifi">Sci-Fi</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="childrens"
+                        name="umbrellaGenre"
+                        value="childrens"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "childrens"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="childrens">Children's</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="drama"
+                        name="umbrellaGenre"
+                        value="drama"
+                        checked={authorListing.umbrellaGenre.includes("drama")}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="drama">Drama</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="horror"
+                        name="umbrellaGenre"
+                        value="horror"
+                        checked={authorListing.umbrellaGenre.includes("horror")}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="horror">Horror</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="comedy"
+                        name="umbrellaGenre"
+                        value="comedy"
+                        checked={authorListing.umbrellaGenre.includes("comedy")}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="comedy">Comedy</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="dystopian"
+                        name="umbrellaGenre"
+                        value="dystopian"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "dystopian"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="dystopian">Dystopian</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="nonfiction"
+                        name="umbrellaGenre"
+                        value="nonfiction"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "nonfiction"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="nonfiction">Non-Fiction</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="western"
+                        name="umbrellaGenre"
+                        value="western"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "western"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="western">Western</label>
+                    </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="historicalfiction"
+                        name="umbrellaGenre"
+                        value="historicalfiction"
+                        checked={authorListing.umbrellaGenre.includes(
+                          "historicalfiction"
+                        )}
+                        onChange={handleGenreChange}
+                      />
+                      <label htmlFor="historicalfiction">Hist. Fict.</label>
+                    </div>
+                  </div>
+                </>
+              )}
+              {/* Subgenre Section */}
+              <label htmlFor="nicheGenre">Subgenre</label>{" "}
               <input
                 type="checkbox"
                 id="nicheGenre"
@@ -154,6 +414,47 @@ function ModifyListing() {
                 checked={dataToModify.nicheGenre}
               />
               <br />
+              {!dataToModify.nicheGenre ? null : (
+                <>
+                  <label htmlFor="subGenre" className="for-accessibility">
+                    Subgenre
+                  </label>
+                  <div>
+                    {/* TODO: adjust styling for input and button */}
+                    <input
+                      className="user-input"
+                      type="text"
+                      name="subGenre"
+                      id="subGenre"
+                      value={userChoice}
+                      onChange={handleUserChoice}
+                    />
+                    {/* adds text to subGenre array */}{" "}
+                    <button onClick={handleAddSubGenre} className="button">
+                      Add
+                    </button>
+                  </div>
+                  {/* display current subGenre array */}
+                  <p>Current Sub-Genres Selected:</p>
+                  <p>
+                    {authorListing.subGenre.map((subGenre, index) => (
+                      <span key={index}>
+                        {subGenre}&nbsp;
+                        {/* removes the selected item from the array */}
+                        <span
+                          className="remove-dash"
+                          onClick={() => handleRemoveSubGenre(index)}
+                        >
+                          &times;
+                        </span>
+                        &nbsp;
+                      </span>
+                    ))}
+                  </p>
+                </>
+              )}
+              {/* Links Section */}
+              {/* TODO: add conditional for links */}
               <label htmlFor="links">Links</label>{" "}
               <input
                 type="checkbox"
@@ -163,6 +464,8 @@ function ModifyListing() {
                 checked={dataToModify.links}
               />
               <br />
+              {/* Bio Section */}
+              {/* TODO: finish logic and settings for textarea */}
               <label htmlFor="authorBio">Bio</label>{" "}
               <input
                 type="checkbox"
@@ -171,6 +474,24 @@ function ModifyListing() {
                 onChange={() => handleChooseToModify("authorBio")}
                 checked={dataToModify.authorBio}
               />
+              {!dataToModify.authorBio ? null : (
+                <>
+                  <label htmlFor="bio" className="for-accessibility">
+                    Bio
+                  </label>
+                  <textarea
+                    name="bio"
+                    id="bio"
+                    cols="30"
+                    rows="10"
+                    defaultValue={authorListing.bio}
+                  ></textarea>
+                  <br />
+                </>
+              )}
+              <div className="submit">
+                <button className="button">Submit</button>
+              </div>
             </div>
           </form>
         </main>
