a1a17698f42631959f4574c9c2dec0a71b7c7225
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-14 15:19:25 -0500

    Update ProcessNewAuthors.jsx
    -adjusted API call to convert JSON strings of umbrellaGenre and subGenre to arrays before saving to the pendingAuthors state
    
    -created function to display formatted umbrella genre string based on genre saved in umbrellaGenre
    
    -started setting up layout for first section displayed on page load

--- src/pages/ProcessNewAuthors.jsx
+++ src/pages/ProcessNewAuthors.jsx
@@ -1,20 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Logout from "../components/Logout";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
-import { useContext } from "react";
+import { ConvertBio } from "../components/ConvertBio";
 import "../styles/Pages.css";
 
 function ProcessNewAuthors() {
   const { userAccessLevel } = useContext(AuthContext);
   const [pendingAuthors, setPendingAuthors] = useState([]);
   const apiAddress = import.meta.env.VITE_API_PENDING_AUTHORS_ADDRESS;
-
-  const unauthorizedUser = () => {
-    if (userAccessLevel !== "admin" && userAccessLevel !== "helper") {
-      window.location.href = "/";
-    }
-  };
 
   useEffect(() => {
     const fetchPendingAuthors = async () => {
@@ -29,7 +23,31 @@ function ProcessNewAuthors() {
         }
 
         const result = await response.json();
-        setPendingAuthors(result.awaitingAuthors);
+        //result returns an object with a success message and an array of objects called awaitingAuthors
+
+        const adjustRawListings = (arrayOfAuthorObjects) => {
+          //create empty array to populate finished list of objects
+          const adjustedListings = [];
+          //go into the array
+          for (let i = 0; i < arrayOfAuthorObjects.length; i++) {
+            //copy original object
+            const author = arrayOfAuthorObjects[i];
+            //convert JSON string to array for umbrellaGenre and subGenre
+            const parseUmbrellaGenre = JSON.parse(author.umbrellaGenre);
+            const parseSubGenre = JSON.parse(author.subGenre);
+            //replace previous values with parsed values
+            author.umbrellaGenre = parseUmbrellaGenre;
+            author.subGenre = parseSubGenre;
+            //add adjusted listing to adjustedListings array
+            adjustedListings.push(author);
+          }
+          return adjustedListings;
+        };
+
+        //assign adjusted results for awaitingAuthors array to finalResult
+        const finalResult = adjustRawListings(result.awaitingAuthors);
+
+        setPendingAuthors(finalResult);
       } catch (error) {
         console.error("Error:", error);
       }
@@ -37,6 +55,43 @@ function ProcessNewAuthors() {
 
     fetchPendingAuthors();
   }, []);
+
+  const properUmbrellaGenreName = (genreValue) => {
+    switch (genreValue) {
+      case "romance":
+        return "Romance";
+      case "fantasy":
+        return "Fantasy";
+      case "thriller":
+        return "Thriller";
+      case "scifi":
+        return "Sci-Fi";
+      case "childrens":
+        return "Children's";
+      case "drama":
+        return "Drama";
+      case "horror":
+        return "Horror";
+      case "comedy":
+        return "Comedy";
+      case "dystopian":
+        return "Dystopian";
+      case "nonfiction":
+        return "Non-Fiction";
+      case "western":
+        return "Western";
+      case "historicalfiction":
+        return "Hist. Fict.";
+      default:
+        return "Not Found";
+    }
+  };
+
+  const unauthorizedUser = () => {
+    if (userAccessLevel !== "admin" && userAccessLevel !== "helper") {
+      window.location.href = "/";
+    }
+  };
 
   return (
     <>
@@ -61,7 +116,120 @@ function ProcessNewAuthors() {
           </p>
         </main>
       ) : (
-        <p>Authors waiting for processing</p>
+        <main className="main">
+          <div className="author-info">
+            <p className="center-text">
+              Verify Accuracy Of All Links And That Author Has Published Work
+            </p>
+            <br />
+            <p>
+              Name: {pendingAuthors[0].firstName} {pendingAuthors[0].lastName}
+            </p>
+            <p>Email: {pendingAuthors[0].email}</p>
+            {pendingAuthors[0].website ? (
+              <p>
+                Website:{" "}
+                <a href={pendingAuthors[0].website}>
+                  {pendingAuthors[0].website}
+                </a>
+              </p>
+            ) : (
+              <p>No Website Found. Verify Membership Before Continuing.</p>
+            )}
+            <p>
+              Umbrella Genre:{" "}
+              {pendingAuthors[0].umbrellaGenre.map((genres, index, array) => (
+                <span key={index}>
+                  {properUmbrellaGenreName(genres)}
+                  {index !== array.length - 1 && ", "}
+                </span>
+              ))}
+            </p>
+            <p>
+              Subgenre:{" "}
+              {pendingAuthors[0].subGenre.map((genres, index, array) => (
+                <span key={index}>
+                  {genres}
+                  {index !== array.length - 1 && ", "}
+                </span>
+              ))}
+            </p>
+            {!pendingAuthors[0].instagram ? null : (
+              <p>
+                Instagram:{" "}
+                <a href={pendingAuthors[0].instagram}>
+                  {pendingAuthors[0].instagram}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].facebook ? null : (
+              <p>
+                Facebook:{" "}
+                <a href={pendingAuthors[0].facebook}>
+                  {pendingAuthors[0].facebook}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].twitter ? null : (
+              <p>
+                Twitter:{" "}
+                <a href={pendingAuthors[0].twitter}>
+                  {pendingAuthors[0].twitter}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].tiktok ? null : (
+              <p>
+                TikTok:{" "}
+                <a href={pendingAuthors[0].tiktok}>
+                  {pendingAuthors[0].tiktok}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].threads ? null : (
+              <p>
+                Threads:{" "}
+                <a href={pendingAuthors[0].threads}>
+                  {pendingAuthors[0].threads}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].mastodon ? null : (
+              <p>
+                Mastodon:{" "}
+                <a href={pendingAuthors[0].mastodon}>
+                  {pendingAuthors[0].mastodon}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].amazonBio ? null : (
+              <p>
+                Amazon Author Page:{" "}
+                <a href={pendingAuthors[0].amazonBio}>
+                  {pendingAuthors[0].amazonBio}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].goodreads ? null : (
+              <p>
+                Goodreads:{" "}
+                <a href={pendingAuthors[0].goodreads}>
+                  {pendingAuthors[0].goodreads}
+                </a>
+              </p>
+            )}
+            {!pendingAuthors[0].bookbub ? null : (
+              <p>
+                BookBub:{" "}
+                <a href={pendingAuthors[0].bookbub}>
+                  {pendingAuthors[0].bookbub}
+                </a>
+              </p>
+            )}
+            <p>Bio: {ConvertBio(pendingAuthors[0].bio)}</p>
+          </div>
+          <form className="author-info">Place Saver</form>
+        </main>
       )}
     </>
   );
